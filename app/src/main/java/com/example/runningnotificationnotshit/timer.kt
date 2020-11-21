ac72512bb215a8140659cea8dfe1e2826f3d10cd
ZinkRuben <zinkruben@protonmail.com>
2020-11-21 20:56:18 +0100

    jejjejjejejejejej

--- app/src/main/java/com/example/runningnotificationnotshit/timer.kt
+++ app/src/main/java/com/example/runningnotificationnotshit/timer.kt
@@ -2,10 +2,31 @@ package com.example.runningnotificationnotshit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.util.Log
+import android.widget.Chronometer
+import kotlinx.android.synthetic.main.activity_timer.*
 
 class timer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timer)
+        startTimer(1,1,1)
+    }
+ //   var view_timerxd = findViewById<Chronometer>(R.id.view_timer)
+    fun startTimer(numberOfPeriods: Int, sprintLength: Int, warmupLength: Int) {
+
+        view_timer.start()
+        view_timer.isCountDown = true
+        view_timer.base = SystemClock.elapsedRealtime() + 6000
+        var elapsedMillis = SystemClock.elapsedRealtime()
+        view_timer.setOnChronometerTickListener {
+            //ez nem mukodik nem ertem viszont hogy miert nem, lol
+            Log.e("alma ", elapsedMillis.toString())
+        }
+
+
+
+
     }
 }

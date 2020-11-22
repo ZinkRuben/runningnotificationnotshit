3452d4737212593bc440bc6a3da9ef6995148307
ZinkRuben <zinkruben@protonmail.com>
2020-11-22 21:26:40 +0100

    majdnem kesz, mar csak az input kell nekem

--- app/src/main/java/com/example/runningnotificationnotshit/timer.kt
+++ app/src/main/java/com/example/runningnotificationnotshit/timer.kt
@@ -2,31 +2,99 @@ package com.example.runningnotificationnotshit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.SystemClock
 import android.util.Log
-import android.widget.Chronometer
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_timer.*
+import java.util.*
+import kotlin.concurrent.schedule
+import kotlin.reflect.typeOf
 
 class timer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timer)
-        startTimer(1,1,1)
-    }
- //   var view_timerxd = findViewById<Chronometer>(R.id.view_timer)
-    fun startTimer(numberOfPeriods: Int, sprintLength: Int, warmupLength: Int) {
 
-        view_timer.start()
-        view_timer.isCountDown = true
-        view_timer.base = SystemClock.elapsedRealtime() + 6000
-        var elapsedMillis = SystemClock.elapsedRealtime()
-        view_timer.setOnChronometerTickListener {
-            //ez nem mukodik nem ertem viszont hogy miert nem, lol
-            Log.e("alma ", elapsedMillis.toString())
+
+        fulltimer(12,6,3)
+
+    }
+    var x = 0
+    var cdFrom = 12
+    var tasksCompleted = 0
+
+    fun timerke() {
+
+        if (x<cdFrom) {
+            Timer("c", false).schedule(1000) {
+                var value = cdFrom - x
+                x += 1
+                runOnUiThread(java.lang.Runnable {
+                    countdownTextView.text = value.toString()
+                })
+
+                timerke()
+            }
+        }
+        else{
+            tasksCompleted += 1
+            x = 0
+            fulltimer(12,6,3)
         }
 
+    }
 
+
+
+    fun fulltimer(warmup: Int, sprint: Int, howManySprint: Int ) {
+
+    if (tasksCompleted == 0) {
+    //warmup
+        cdFrom = warmup
+        timerke()
+    }
+
+            //sprinting parts
+            for(y in 0..(howManySprint-1)) {
+                //sprint
+                if (tasksCompleted == 1 + 2*y) {
+                    cdFrom = sprint
+                    timerke()
+                }
+                if (tasksCompleted == 2 + 2*y) {
+                    //jogging
+                    cdFrom = (10 - sprint)
+                    timerke()
+
+                }
+            }
+
+
+
+
+
+
+
+
+
+}
 
 
     }
-}
+/*
+    fun startTimer(numberOfPeriods: Int, sprintLength: Int, warmupLength: Int) {
+
+
+        view_timer.start()
+        view_timer.base = SystemClock.elapsedRealtime() + 6000
+        view_timer.setCountDown(true)
+        view_timer.setOnChronometerTickListener {
+            var elapsedMillis = SystemClock.elapsedRealtime()
+            //ez nem mukodik nem ertem viszont hogy miert nem, lol
+            Log.e("alma ", elapsedMillis.toString())
+            timer
+        }
+
+    }
+
+
+    */
